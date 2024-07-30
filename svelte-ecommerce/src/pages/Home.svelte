4a1f6b84b6ed36aaa686b6a9ad750ff374098240
Software Engineer 16 <Software.Engineer16@bafokeng.local>
2024-07-30 15:39:28 +0200

    Create Home.svelte

---
+++ svelte-ecommerce/src/pages/Home.svelte
@@ -0,0 +1,11 @@
+<script>
+    import FilterSort from "../components/FilterSort.svelte";
+    import ProductsList from "../components/ProductsList.svelte";
+</script>
+
+<main class="bg-[#81ccdd]">
+    <div>
+        <FilterSort />
+    </div>
+    <ProductsList />
+</main>
